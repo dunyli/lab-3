7c5cfc96be333b21b75b103f624e0527ab753bea
dns <dynuli01@mail.com>
2024-12-19 14:06:44 +0700

    Создание класса Product

--- Lab3/Header.h
+++ Lab3/Header.h
@@ -10,3 +10,30 @@ public:
     void set(char* name, char* phone, char* address); //Получение значений переменных-членов класса
     void get(char* name, char* phone, char* address); //Получение данных класса
 };
+
+//Класс для хранения информации о товаре
+class Product {
+private:
+    std::string name;
+    int price;
+    int count;
+    int category_id;
+    int supplier_id;
+public:
+    Product(); //конструктор
+    Product(Product& p) {
+        name = p.name;
+        price = p.price;
+        count = p.count;
+        category_id = p.category_id;
+        supplier_id = p.supplier_id;
+    }
+    void get(std::string& name, int* price, int* count, int* category_id, int* supplier_id); //Получение данных класса
+    void set(std::string name, int price, int count, int category_id, int supplier_id); //Получение значений переменных-членов класса
+    int getPrice() {
+        return price;
+    };
+    int getCount() {
+        return count;
+    };
+};
