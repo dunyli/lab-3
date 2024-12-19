d715ad1e4e998f492427b46f54a9ebf37ce48788
dns <dynuli01@mail.com>
2024-12-19 14:05:47 +0700

    Создание класса Client

--- Lab3/Header.h
+++ Lab3/Header.h
@@ -1,1 +1,12 @@
 #pragma once
+//Класс для хранения данных о клиенте
+class Client {
+private:
+    char name[50];
+    char phone[20];
+    char address[100];
+public:
+    Client(char* name, char* phone, char* address); //конструктор с параметрами
+    void set(char* name, char* phone, char* address); //Получение значений переменных-членов класса
+    void get(char* name, char* phone, char* address); //Получение данных класса
+};
